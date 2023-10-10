79ccc6c83acd04eacf0eb9cceca87d05a5f5854b
hotlanh <106688624+hotlanh@users.noreply.github.com>
2023-10-10 22:16:33 +0700

    Update vpn4g.sh

--- vpn4g.sh
+++ vpn4g.sh
@@ -17,7 +17,6 @@ pre_install(){
  
   echo -e "  [1] V2ray"
   echo -e "  [2] Trojan"
-  echo -e "  [3] Shadowsocks"
    read -p "  Nhập loại Node: " NodeType
   if [ "$NodeType" == "1" ]; then
     NodeType="V2ray"
@@ -76,8 +75,6 @@ clone_node(){
     NodeType="V2ray"
   elif [ "$NodeType" == "2" ]; then
     NodeType="Trojan"
-  elif [ "$NodeType" == "3" ]; then
-    NodeType="Shadowsocks"
   else 
    NodeType="V2ray"
   fi
