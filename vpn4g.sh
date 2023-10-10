a99c9787c5807bc62163a5ce03a523d6e2ccabaa
hotlanh <106688624+hotlanh@users.noreply.github.com>
2023-10-10 22:08:31 +0700

    Update vpn4g.sh

--- vpn4g.sh
+++ vpn4g.sh
@@ -1,188 +1,248 @@
-#!/binstall
+#!/bin/bash
+clear
+echo ""
+echo "   1. Cài đặt"
+echo "   2. update config"
+echo "   3. thêm node"
+read -p "   Lựa chọn của bạn là (mặc định Cài đặt): " num
+[ -z "${num}" ] && num="1"
+    
+pre_install(){
+ clear
+    read -p "  Nhập số Node cần cài (mặc định 1): " n
+     [ -z "${n}" ] && n="1"
+    a=0
+  while [ $a -lt $n ]
+ do
+ 
+  echo -e "  [1] V2ray"
+  echo -e "  [2] Trojan"
+   read -p "  Nhập loại Node: " NodeType
+  if [ "$NodeType" == "1" ]; then
+    NodeType="V2ray"
+  elif [ "$NodeType" == "2" ]; then
+    NodeType="Trojan"
+  else 
+   NodeType="V2ray"
+  fi
+  echo "-------------------------------"
+  echo -e "  Loại Node là ${NodeType}"
+  echo "-------------------------------"
 
-# azz
-red() {
-	echo -e "\033[31m\033[01m$1\033[0m"
-}
 
-green() {
-	echo -e "\033[32m\033[01m$1\033[0m"
-}
+  #node id
+    read -p "  Nhập ID Node: " node_id
+  [ -z "${node_id}" ] && node_id=0
+  echo "-------------------------------"
+  echo -e "  ID Node là ${node_id}"
+  echo "-------------------------------"
+  
 
-yellow() {
-	echo -e "\033[33m\033[01m$1\033[0m"
-}
+  #giới hạn thiết bị
+read -p "  Nhập giới hạn thiết bị: " DeviceLimit
+  [ -z "${DeviceLimit}" ] && DeviceLimit="0"
+  echo "-------------------------------"
+  echo "  Thiết bị tối đa là ${DeviceLimit}"
+  echo "-------------------------------"
+  
+  
+  #IP vps
+ read -p "  Nhập địa chỉ Node: " CertDomain
+  [ -z "${CertDomain}" ] && CertDomain="0"
+ echo "-------------------------------"
+  echo "  Địa chỉ Node là ${CertDomain}"
+ echo "-------------------------------"
 
-# azz
-REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'")
-RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Alpine")
-PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update")
-PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "apk add -f")
-PACKAGE_REMOVE=("apt -y remove" "apt -y remove" "yum -y remove" "yum -y remove")
-
-CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
-
-for i in "${CMD[@]}"; do
-	SYS="$i" && [[ -n $SYS ]] && break
+ config
+  a=$((a+1))
 done
-
-for ((int = 0; int < ${#REGEX[@]}; int++)); do
-	[[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
-done
-
-[[ -z $SYSTEM ]] && red "ẻ e" && exit 1
-
-archAffix() {
-	case "$(uname -m)" in
-  x86_64 | x64 | amd64) return 0 ;;
-	aarch64 | arm64) return 0 ;;
-	*) red "ẻ e k việt sub！" ;;
-	esac
-
-	return 0
 }
 
-install() {
-	install_XrayR
-	clear
-	makeConfig
+
+#clone node
+clone_node(){
+  clear
+    read -p "  Nhập số Node cần cài thêm (mặc định 1): " n
+     [ -z "${n}" ] && n="1"
+    a=0
+  while [ $a -lt $n ]
+  do
+  
+  #link web 
+   read -p "  Nhập domain web (không cần https://): " api_host
+    [ -z "${api_host}" ] && api_host=0
+    echo "--------------------------------"
+  echo "  Web của bạn là https://${api_host}"
+  echo "--------------------------------"
+  #key web
+  read -p "  Nhập key của web: " api_key
+    [ -z "${api_key}" ] && api_key=0
+  echo "--------------------------------"
+  echo "  Key của web là ${api_key}"
+  echo "--------------------------------"
+
+  echo -e "  [1] V2ray"
+  echo -e "  [2] Trojan"
+   read -p "  Nhập loại Node: " NodeType
+  if [ "$NodeType" == "1" ]; then
+    NodeType="V2ray"
+  elif [ "$NodeType" == "2" ]; then
+    NodeType="Trojan"
+  else 
+   NodeType="V2ray"
+  fi
+  echo "-------------------------------"
+  echo -e "  Loại Node là ${NodeType}"
+  echo "-------------------------------"
+
+  #node id
+    read -p "  Nhập ID Node: " node_id
+  [ -z "${node_id}" ] && node_id=0
+  echo "-------------------------------"
+  echo -e "  ID Node là ${node_id}"
+  echo "-------------------------------"
+  
+
+  #giới hạn thiết bị
+read -p "  Nhập giới hạn thiết bị: " DeviceLimit
+  [ -z "${DeviceLimit}" ] && DeviceLimit="0"
+  echo "-------------------------------"
+  echo "  Thiết bị tối đa là ${DeviceLimit}"
+  echo "-------------------------------"
+  
+  #IP vps
+ read -p "  Nhập địa chỉ Node: " CertDomain
+  [ -z "${CertDomain}" ] && CertDomain="0"
+ echo "-------------------------------"
+  echo "  Địa chỉ Node là ${CertDomain}"
+ echo "-------------------------------"
+
+ config
+  a=$((a+1))
+  done
 }
 
-install_XrayR() {
-	[[ -z $(type -P curl) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} curl
-	[[ -z $(type -P socat) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} socat
-	bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
-}
 
-makeConfig() {
-    echo "------  Config 80 & 443 BY Tao ---------"
-	read -p "Loại website của bạn: V2board"
-	echo "---------------"
-	read -p "Link website: https://vpn4g.xyz/"
-	echo "---------------"
-	read -p "API key của web: khongbietdiencaigibaygio"
-	echo "---------------"
-	read -p "Node ID 80: " NodeID80
-	echo -e "Node 80 là: ${NodeID80}"
-	echo "---------------"
-	read -p "Nhập CertDomain port 80: " CertDomain80
-  echo -e "CertDomain là: ${CertDomain80}"
-  echo "---------------"
-	read -p "Node ID 443: " NodeID443
-	echo -e "Node 80 là: ${NodeID443}"
-	echo "---------------"
-  read -p "Nhập CertDomain port 443: " CertDomain443
-  echo -e "CertDomain là: ${CertDomain443}"
-	echo "---------------"
-  read -p "Nhập limit Device: " limitDevice
-  echo -e "Limit Device là: ${limitDevice}"
-	echo "---------------"
 
-	rm -f /etc/XrayR/config.yml
-	if [[ -z $(~/.acme.sh/acme.sh -v 2>/dev/null) ]]; then
-		curl https://get.acme.sh | sh -s email=script@github.com
-		source ~/.bashrc
-		bash ~/.acme.sh/acme.sh --upgrade --auto-upgrade
-	fi
-         cat <<EOF >/etc/XrayR/config.yml
-Log:
-  Level: none 
-  AccessPath: # /etc/XrayR/access.Log
-  ErrorPath: # /etc/XrayR/error.log
-DnsConfigPath: # /etc/XrayR/dns.json
-InboundConfigPath: # /etc/XrayR/custom_inbound.json
-RouteConfigPath: # /etc/XrayR/route.json
-OutboundConfigPath: # /etc/XrayR/custom_outbound.json
-ConnetionConfig:
-  Handshake: 4 
-  ConnIdle: 30 
-  UplinkOnly: 2 
-  DownlinkOnly: 4 
-  BufferSize: 64 
-Nodes:
+
+
+
+
+config(){
+cd /etc/XrayR
+cat >>config.yml<<EOF
   -
-    PanelType: "V2board" 
+    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel, V2RaySocks
     ApiConfig:
       ApiHost: "https://vpn4g.xyz"
       ApiKey: "khongbietdiencaigibaygio"
-      NodeID: $NodeID80
-      NodeType: V2ray 
-      Timeout: 30 
-      EnableVless: false 
-      EnableXTLS: false 
-      SpeedLimit: 0 
-      DeviceLimit: $limitDevice
-      RuleListPath: # /etc/XrayR/rulelist
+      NodeID: $node_id
+      NodeType: $NodeType # Node type: V2ray, Shadowsocks, Trojan, Shadowsocks-Plugin
+      Timeout: 30 # Timeout for the api request
+      EnableVless: false # Enable Vless for V2ray Type
+      EnableXTLS: false # Enable XTLS for V2ray and Trojan
+      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: $DeviceLimit # Local settings will replace remote settings, 0 means disable
+      RuleListPath: # /etc/XrayR/rulelist Path to local rulelist file
     ControllerConfig:
-      DisableSniffing: True
-      ListenIP: 0.0.0.0 
-      SendIP: 0.0.0.0 
-      UpdatePeriodic: 60 
-      EnableDNS: false 
-      DNSType: AsIs 
-      EnableProxyProtocol: false 
-      EnableFallback: false 
-      FallBackConfigs:  
+      ListenIP: 0.0.0.0 # IP address you want to listen
+      SendIP: 0.0.0.0 # IP address you want to send pacakage
+      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
+      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
+      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
+      DisableUploadTraffic: false # Disable Upload Traffic to the panel
+      DisableGetRule: false # Disable Get Rule from the panel
+      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
+      DisableSniffing: True # Disable domain sniffing
+      EnableProxyProtocol: false # Only works for WebSocket and TCP
+      AutoSpeedLimitConfig:
+        Limit: 0 # Warned speed. Set to 0 to disable AutoSpeedLimit (mbps)
+        WarnTimes: 0 # After (WarnTimes) consecutive warnings, the user will be limited. Set to 0 to punish overspeed user immediately.
+        LimitSpeed: 0 # The speedlimit of a limited user (unit: mbps)
+        LimitDuration: 0 # How many minutes will the limiting last (unit: minute)
+      GlobalDeviceLimitConfig:
+        Limit: 0 # The global device limit of a user, 0 means disable
+        RedisAddr: 127.0.0.1:6379 # The redis server address
+        RedisPassword: YOUR PASSWORD # Redis password
+        RedisDB: 0 # Redis DB
+        Timeout: 5 # Timeout for redis request
+        Expiry: 60 # Expiry time (second)
+      EnableFallback: false # Only support for Trojan and Vless
+      FallBackConfigs:  # Support multiple fallbacks
         -
-          SNI: 
-          Path: 
-          Dest: 80 
-          ProxyProtocolVer: 0 
+          SNI: # TLS SNI(Server Name Indication), Empty for any
+          Alpn: # Alpn, Empty for any
+          Path: # HTTP PATH, Empty for any
+          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/features/fallback.html for details.
+          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
       CertConfig:
-        CertMode: http
-        CertDomain: "$CertDomain80" 
-        CertFile: /etc/XrayR/cert/vpn4g.crt
-        KeyFile: /etc/XrayR/cert/vpn4g.key
-        Provider: alidns 
+        CertMode: file # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
+        CertDomain: "$CertDomain" # Domain to cert
+        CertFile: /etc/XrayR/443.crt # Provided if the CertMode is file
+        KeyFile: /etc/XrayR/443.key
+        Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
         Email: test@me.com
-        DNSEnv: 
+        DNSEnv: # DNS ENV option used by DNS provider
           ALICLOUD_ACCESS_KEY: aaa
           ALICLOUD_SECRET_KEY: bbb
-  -
-    PanelType: "V2board" 
-    ApiConfig:
-      ApiHost: "https://vpn4g.xyz"
-      ApiKey: "khongbietdiencaigibaygio"
-      NodeID: $NodeID443
-      NodeType: V2ray 
-      Timeout: 30 
-      EnableVless: false 
-      EnableXTLS: false 
-      SpeedLimit: 0 
-      DeviceLimit: $limitDevice
-      RuleListPath: # /etc/XrayR/rulelist
-    ControllerConfig:
-      DisableSniffing: True
-      ListenIP: 0.0.0.0 
-      SendIP: 0.0.0.0 
-      UpdatePeriodic: 60 
-      EnableDNS: false 
-      DNSType: AsIs 
-      EnableProxyProtocol: false 
-      EnableFallback: false 
-      FallBackConfigs:  
-        -
-          SNI: 
-          Path: 
-          Dest: 80 
-          ProxyProtocolVer: 0 
-      CertConfig:
-        CertMode: file 
-        CertDomain: "$CertDomain443"
-        CertFile: /etc/XrayR/cert/vpn4g.crt 
-        KeyFile: /etc/XrayR/cert/vpn4g.key
-        Provider: cloudflare 
-        Email: test@me.com
-        DNSEnv: 
-          CLOUDFLARE_EMAIL: 
-          CLOUDFLARE_API_KEY: 
-
 EOF
-	cd /etc/XrayR
-	git clone https://github.com/hotlanh/cert.git
-	XrayR restart
-	green "Đã xong, reboot nếu k thành công！"
-	exit 1
-}
 
-install
+#   sed -i "s|ApiHost: \"https://domain.com\"|ApiHost: \"${api_host}\"|" ./config.yml
+ # sed -i "s|ApiKey:.*|ApiKey: \"${ApiKey}\"|" 
+#   sed -i "s|NodeID: 41|NodeID: ${node_id}|" ./config.yml
+#   sed -i "s|DeviceLimit: 0|DeviceLimit: ${DeviceLimit}|" ./config.yml
+#   sed -i "s|SpeedLimit: 0|SpeedLimit: ${SpeedLimit}|" ./config.yml
+#   sed -i "s|CertDomain:\"node1.test.com\"|CertDomain: \"${CertDomain}\"|" ./config.yml
+ }
+
+case "${num}" in
+1) bash <(curl -Ls https://raw.githubusercontent.com/hotlanh/123/main/install.sh)
+openssl req -newkey rsa:2048 -x509 -sha256 -days 365 -nodes -out /etc/XrayR/443.crt -keyout /etc/XrayR/443.key -subj "/C=JP/ST=Tokyo/L=Chiyoda-ku/O=Google Trust Services LLC/CN=google.com"
+cd /etc/XrayR
+  cat >config.yml <<EOF
+Log:
+  Level: none # Log level: none, error, warning, info, debug 
+  AccessPath: # /etc/XrayR/access.Log
+  ErrorPath: # /etc/XrayR/error.log
+DnsConfigPath: # /etc/XrayR/dns.json # Path to dns config, check https://xtls.github.io/config/dns.html for help
+RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/routing.html for help
+OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/outbound.html for help
+ConnectionConfig:
+  Handshake: 4 # Handshake time limit, Second
+  ConnIdle: 30 # Connection idle time limit, Second
+  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
+  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
+  BufferSize: 64 # The internal cache size of each connection, kB  
+Nodes:
+EOF
+pre_install
+cd /root
+xrayr start
+ ;;
+ 2) cd /etc/XrayR
+cat >config.yml <<EOF
+Log:
+  Level: none # Log level: none, error, warning, info, debug 
+  AccessPath: # /etc/XrayR/access.Log
+  ErrorPath: # /etc/XrayR/error.log
+DnsConfigPath: # /etc/XrayR/dns.json # Path to dns config, check https://xtls.github.io/config/dns.html for help
+RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/routing.html for help
+OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/outbound.html for help
+ConnectionConfig:
+  Handshake: 4 # Handshake time limit, Second
+  ConnIdle: 30 # Connection idle time limit, Second
+  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
+  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
+  BufferSize: 64 # The internal cache size of each connection, kB 
+Nodes:
+EOF
+pre_install
+cd /root
+xrayr restart
+ ;;
+ 3) cd /etc/XrayR
+ clone_node
+ cd /root
+  xrayr restart
+;;
+esac
